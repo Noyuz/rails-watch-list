a0e6b251951ecf059a992c9d261cad6de3979056
Kevin Abergel <k.abergel91@gmail.com>
2021-07-31 12:45:46 +0200

    changing require in application.js

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  const element = document.querySelector('.select2');
+  if (element) {
+    $('.select2').select2();
+  }
 };
 
 export { initSelect2 };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
 import 'bootstrap'
 import { initSelect2 } from '../components/init_select2';
 import AOS from 'aos';
@@ -20,7 +22,3 @@ document.addEventListener("turbolinks:load", () => {
       LoadDynamicBannerText();
     }
   });
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
